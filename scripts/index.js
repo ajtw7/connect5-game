d644d37d6757c9bd4ff37f740d9c2d9552921ea2
Andrew <ayjtw7@gmail.com>
2022-08-29 08:45:00 -0400

    initialized structure of the tower. 2D array (7x8)

---
+++ scripts/index.js
@@ -0,0 +1,234 @@
+let tower = [
+    row1 = [{
+            positionX: 0,
+            positionY: 0
+        },
+        {
+            positionX: 0,
+            positionY: 1
+        },
+        {
+            positionX: 0,
+            positionY: 2
+        },
+        {
+            positionX: 0,
+            positionY: 3
+        },
+        {
+            positionX: 0,
+            positionY: 4
+        },
+        {
+            positionX: 0,
+            positionY: 5
+        },
+        {
+            positionX: 0,
+            positionY: 6
+        },
+        {
+            positionX: 0,
+            positionY: 7
+        }
+    ],
+    row2 = [{
+            positionX: 1,
+            positionY: 0
+        },
+        {
+            positionX: 1,
+            positionY: 1
+        }, {
+            positionX: 1,
+            positionY: 2
+        }, {
+            positionX: 1,
+            positionY: 3
+        }, {
+            positionX: 1,
+            positionY: 4
+        }, {
+            positionX: 1,
+            positionY: 5
+        }, {
+            positionX: 1,
+            positionY: 6
+        },
+        {
+            positionX: 1,
+            positionY: 7
+        }
+    ],
+
+    row3 = [{
+            positionX: 2,
+            positionY: 0
+        },
+        {
+            positionX: 2,
+            positionY: 1
+        },
+        {
+            positionX: 2,
+            positionY: 2
+        },
+        {
+            positionX: 2,
+            positionY: 3
+        },
+        {
+            positionX: 2,
+            positionY: 4
+        },
+        {
+            positionX: 2,
+            positionY: 5
+        },
+        {
+            positionX: 2,
+            positionY: 6
+        },
+        {
+            positionX: 2,
+            positionY: 7
+        }
+
+    ],
+    row4 = [{
+            positionX: 3,
+            positionY: 0
+        },
+        {
+            positionX: 3,
+            positionY: 1
+        },
+        {
+            positionX: 3,
+            positionY: 2
+        },
+        {
+            positionX: 3,
+            positionY: 3
+        },
+        {
+            positionX: 3,
+            positionY: 4
+        },
+        {
+            positionX: 3,
+            positionY: 5
+        },
+        {
+            positionX: 3,
+            positionY: 6
+        },
+        {
+            positionX: 3,
+            positionY: 7
+        }
+    ],
+
+    row5 = [{
+            positionX: 4,
+            positionY: 0
+        },
+        {
+            positionX: 4,
+            positionY: 1
+        },
+        {
+            positionX: 4,
+            positionY: 2
+        },
+        {
+            positionX: 4,
+            positionY: 3
+        },
+        {
+            positionX: 4,
+            positionY: 4
+        },
+        {
+            positionX: 4,
+            positionY: 5
+        },
+        {
+            positionX: 4,
+            positionY: 6
+        },
+        {
+            positionX: 4,
+            positionY: 7
+        }
+    ],
+
+    row6 = [{
+            positionX: 5,
+            positionY: 0
+        },
+        {
+            positionX: 5,
+            positionY: 1
+        },
+        {
+            positionX: 5,
+            positionY: 2
+        },
+        {
+            positionX: 5,
+            positionY: 3
+        },
+        {
+            positionX: 5,
+            positionY: 4
+        },
+        {
+            positionX: 5,
+            positionY: 5
+        },
+        {
+            positionX: 5,
+            positionY: 6
+        },
+        {
+            positionX: 5,
+            positionY: 7
+        }
+    ],
+
+    row7 = [{
+            positionX: 6,
+            positionY: 0
+        },
+        {
+            positionX: 6,
+            positionY: 1
+        },
+        {
+            positionX: 6,
+            positionY: 2
+        },
+        {
+            positionX: 6,
+            positionY: 3
+        },
+        {
+            positionX: 6,
+            positionY: 4
+        },
+        {
+            positionX: 6,
+            positionY: 5
+        },
+        {
+            positionX: 6,
+            positionY: 6
+        },
+        {
+            positionX: 6,
+            positionY: 7
+        }
+    ],
+
+]
