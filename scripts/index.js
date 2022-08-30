2fac2873f85978bed2cbd9f487646f6a8da65774
Andrew <ayjtw7@gmail.com>
2022-08-30 11:07:54 -0400

    adding functionality to click event listener that places shapes in exact coordinates of grid

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("#myCanvas")
 // console.log(canvas)
 const ctx = canvas.getContext('2d')
+ctx.font = '30px Arial'
 
 // Initializing the main game space with 7 columns and 8 rows (56 positions).
 console.log('WASSUPPP')
@@ -14,7 +15,17 @@ let towerArray = [
     [null, null, null, null, null, null, null, null],
 ]
 
-console.log(towerArray[2][0])
+// console.log(towerArray[2][0])
+
+// const intervalId = setInterval(function () {
+//     // loop through 2D array
+//     console.log('hi')
+//     for (let i = 0; i < towerArray.length; i++) {
+//         for (let j = 0; j < towerArray[i].length; j++) {
+//             console.log(towerArray[i][j])
+//         }
+//     }
+// }, 100000)
 
 // for loop to draw vertical lines
 for (let x = 0; x < towerArray.length; x++) {
@@ -35,6 +46,8 @@ for (let y = 0; y < towerArray[0].length; y++) {
     stokeLineVert(ctx, y + 1)
 }
 
+
+//Canvas circle 
 function stokeLineVert(ctx, y) {
     ctx.beginPath();
     ctx.moveTo(0, y * 71.4);
@@ -43,29 +56,16 @@ function stokeLineVert(ctx, y) {
     ctx.closePath();
 }
 
-let circleWidth = (canvas.width / 8) / 2;
-let circleHeight = (canvas.height / 7) / 2;
+// let circleWidth = (canvas.width / 8) / 2;
+// let circleHeight = (canvas.height / 7) / 2;
 
-//Canvas circle 
-ctx.beginPath();
-ctx.arc(circleWidth, circleHeight, 25, 0, Math.PI * 2);
-ctx.fillStyle = 'blue';
-ctx.fill();
-ctx.stroke();
+// ctx.beginPath();
+// ctx.arc(circleWidth, circleHeight, 25, 0, Math.PI * 2);
+// ctx.fillStyle = 'blue';
+// ctx.fill();
+// ctx.stroke();
 
 // --------------------
-
-for (let i = 0; i < towerArray.length; i++) {
-    for (let j = 0; j < towerArray[i].length; j++) {
-        console.log(towerArray[i][j])
-        
-
-
-
-
-
-    }
-}
 
 // Initializing both players
 const playerOne = {
@@ -96,30 +96,63 @@ const scoreboard = {
 
 
 
-/* 
-Conditionals
+// "Click" event listener to add shapes into spaces
 
-1) if playerOne, drops a medallion, into a space, playerTwo must go next.
-        1a) scoreboard must be updated
-
-2) if any column/row is full, player must drop medallion elsewhere.
-    2a) if a space is occupied, it must be flipped from "null" to TRUE 
-
-3) if player positions 5 medallions of the same color in a row, said player win the game
-
-4) if medallionsPlayed = 56 or if spacesRemaining = 0, the game is over
-
-5)
-
-
-
-*/
-
-
-canvas.addEventListener('click', function(event) {
+canvas.addEventListener('click', function (event) {
     console.log('clicked in browser', event.clientX, event.clientY)
     let {x,y} = canvas.getBoundingClientRect()
     console.log('canvas top left corner', x, y)
     console.log('where we clicked in canvas', event.clientX - x, event.clientY - y)
-    console.log('index of array', Math.floor((event.clientY - y) / 71.4), Math.floor((event.clientX - x)/100))
+    console.log('index of array', Math.floor((event.clientY - y) / 71.4), Math.floor((event.clientX - x) / 100))
+
+    // Canvas coordinates
+    let canvasCoordX = event.clientX - x;
+    let canvasCoordY = event.clientY - y;
+
+    // Array coordinates
+    let arrayX = Math.floor((event.clientX - x) / 100)
+    let arrayY = Math.floor((event.clientY - y) / 71.4)
+
+
+    if (arrayX === 0 && arrayY === 5 || arrayX === 6 && arrayY === 5) {
+        console.log('yes')
+        ctx.beginPath();
+        ctx.arc(canvasCoordX, canvasCoordY, 25, 0, Math.PI * 2);
+        ctx.fillStyle = 'blue';
+        ctx.fill();
+        ctx.stroke();
+    } else {
+        console.log('no')
+        ctx.beginPath();
+        ctx.arc(canvasCoordX, canvasCoordY, 25, 0, Math.PI * 2);
+        ctx.fillStyle = 'red';
+        ctx.fill();
+        ctx.stroke();
+    }
+
+
+
+
+
+
+
 })
+
+/* 
+    Conditionals
+    
+    1) if playerOne, drops a medallion, into a space, playerTwo must go next.
+            1a) scoreboard must be updated
+    
+    2) if any column/row is full, player must drop medallion elsewhere.
+        2a) if a space is occupied, it must be flipped from "null" to TRUE 
+    
+    3) if player positions 5 medallions of the same color in a row, said player win the game
+    
+    4) if medallionsPlayed = 56 or if spacesRemaining = 0, the game is over
+    
+    5)
+    
+    
+    
+    */
