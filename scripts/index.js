a64312c08ed9beb0d7a248fe9fc57592c287a5d9
Andrew <ayjtw7@gmail.com>
2022-08-30 18:00:10 -0400

    algorithm to check if player wins after adding 5 in a row

--- scripts/index.js
+++ scripts/index.js
@@ -83,10 +83,23 @@ class medallion {
 
 const scoreboard = {
     medallionsPlayed: 0,
-    spacesRemaining: 56
+    spacesRemaining: 56,
+    update: function() {
+
+    }
 }
 
-
+function checkForWin(x,y,color){
+    if(!(y + 5 > towerArray.length) && !(x + 5 > towerArray[y].length)){ //check left-right diagonal
+      for(let c = 0; c < 5; c++){
+        if(towerArray[y + c][x + c] != color){
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
 
 // "Click" event listener to add shapes into spaces
 let currentTurn = 'blue';
@@ -97,79 +110,78 @@ canvas.addEventListener('click', function (event) {
     console.log('where we clicked in canvas', event.clientX - x, event.clientY - y)
     console.log('index of array', Math.floor((event.clientY - y) / 71.4), Math.floor((event.clientX - x) / 100))
 
-    // Canvas coordinates
-    let canvasCoordX = event.clientX - x;
-    let canvasCoordY = event.clientY - y;
-
     // Array coordinates
     let arrayX = Math.floor((event.clientX - x) / 100)
     let arrayY = Math.floor((event.clientY - y) / 71.4)
 
+    // If it is player 1's turn, alert player 2 they are next
     if(currentTurn == 'blue'){
         towerArray[arrayY][arrayX] = 'blue'
+        for (let i = 0; i < towerArray.length; i++) {
+            for (let j = 0; j < towerArray[i].length; j++) {
+                if(checkForWin(j, i, 'blue')){
+                    alert('blue wins')
+                }
+            }
+        }
         currentTurn = 'red'
         alert("now red's turn")
     } else {
+    // If it is player 2's turn, alert player 1 they are next
         towerArray[arrayY][arrayX] = 'red'
+        for (let i = 0; i < towerArray.length; i++) {
+            for (let j = 0; j < towerArray[i].length; j++) {
+                if(checkForWin(j, i, 'red')){
+                    alert('red wins')
+                }
+            }
+        }
         currentTurn = 'blue'
         alert("now blue's turn")
     }
     
     console.log(towerArray)
-
-
-    // if (arrayX === 0 && arrayY === 5 || arrayX === 6 && arrayY === 5) {
-    //     console.log('yes')
-    //     ctx.beginPath();
-    //     ctx.arc(canvasCoordX, canvasCoordY, 25, 0, Math.PI * 2);
-    //     ctx.fillStyle = 'blue';
-    //     ctx.fill();
-    //     ctx.stroke();
-    // } else {
-    //     console.log('no')
-    //     ctx.beginPath();
-    //     ctx.arc(canvasCoordX, canvasCoordY, 25, 0, Math.PI * 2);
-    //     ctx.fillStyle = 'red';
-    //     ctx.fill();
-    //     ctx.stroke();
-    // }
-
-
-
-
-
-
-
 })
 
 const intervalId = setInterval(function () {
-    // loop through 2D array
     console.log('hi')
+    // clear the canvas
     ctx.clearRect(0,0,canvas.width, canvas.height)
+
+    // redraw the lines after ever clearance
     for (let x = 0; x < towerArray.length; x++) {
         strokeLineHoriz(ctx, x + 1) // dont want the array to start at 0
     }
     for (let y = 0; y < towerArray[0].length; y++) {
         stokeLineVert(ctx, y + 1)
     }
+
+    // loop through 2D array
     for (let i = 0; i < towerArray.length; i++) {
         for (let j = 0; j < towerArray[i].length; j++) {
-            if(towerArray[i][j] == 'blue'){
+
+            // if it is player 1's turn, draw a "blue" medallion 
+            if(towerArray[i][j] == 'blue'){  
                 ctx.beginPath();
                 ctx.arc(j * 100 + 50, i * 71.4 + 35.7, 25, 0, Math.PI * 2);
                 ctx.fillStyle = 'blue';
                 ctx.fill();
                 ctx.stroke();
-            } else if (towerArray[i][j] == 'red'){
+            // if it is player 1's turn, draw a "red" medallion
+            } else if (towerArray[i][j] == 'red'){  
                 ctx.beginPath();
                 ctx.arc(j * 100 + 50, i * 71.4 + 35.7, 25, 0, Math.PI * 2);
                 ctx.fillStyle = 'red';
                 ctx.fill();
                 ctx.stroke();
             }
+
+            
+
+            
         }
     }
-}, 1000)
+}, 1000) // cycle every second
 
 /* 
     Conditionals
